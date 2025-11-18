21d48e6071e048a1c18d686e4eb4942922c3a5b0
Lavínia Ferreira <souzlaviniaferreira@gmail.com>
2025-11-18 14:54:53 -0300

    Add files via upload

---
+++ vetor8/vetor8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matriz = { { 4.5, 5.2 }, { 4.2, 1 } };
+            int x, y;
+            Console.WriteLine("\n Imprimindo a matriz:\n");
+            for (x = 0; x < 2; x++)
+            {
+                for (y = 0; y < 2; y++)
+                {
+                    Console.WriteLine("\n mat[{0}][{1}]={2:N1}", x, y, matriz[x, y]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
