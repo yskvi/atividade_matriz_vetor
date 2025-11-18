df7675e06e2c5133c9677bdef7699673bc52a798
Lavínia Ferreira <souzlaviniaferreira@gmail.com>
2025-11-18 14:58:12 -0300

    Add files via upload

---
+++ vetor12/vetor12/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+ 
+                    Random random = new Random();
+
+                    const int QTD_COL = 3;
+                    const int QTD_LIN = 3;
+                    int[,] matriz = new int[QTD_LIN, QTD_COL];
+
+                    matriz = cadastrarNumeros(QTD_LIN, QTD_COL);
+
+                    Console.WriteLine("\n--------MEMORIZE--------\n");
+                    exibirDados(matriz);
+
+                    Thread.Sleep(5000); 
+                    Console.Clear();    
+
+                    
+                    int sorteioLinha = random.Next(0, QTD_LIN); 
+                    int sorteioColuna = random.Next(0, QTD_COL); 
+
+                    Console.Write("\nQual número consta na posição: linha {0}, coluna {1} da matriz: ", sorteioLinha, sorteioColuna);
+                    int num = int.Parse(Console.ReadLine());
+
+                   
+                    if (verifica(matriz[sorteioLinha, sorteioColuna], num))
+                        Console.WriteLine("\nParabéns! Você acertou!\n");
+                    else
+                        Console.WriteLine("\nQue pena! Você errou! Número correto seria: {0}\n", matriz[sorteioLinha, sorteioColuna]);
+
+                    exibirDados(matriz);
+                    Console.ReadKey();
+                }
+
+             
+                static int[,] cadastrarNumeros(int nLin, int nCol)
+                {
+                    int[,] n = new int[nLin, nCol];
+                    Random random = new Random();
+
+                    for (int l = 0; l < nLin; l++)
+                    {
+                        for (int c = 0; c < nCol; c++)
+                        {
+                            n[l, c] = random.Next(0, 50); 
+                        }
+                    }
+                    return n;
+                }
+
+               
+                static bool verifica(int numeroMatriz, int numUsuario)
+                {
+                    return numeroMatriz == numUsuario ? true : false;
+                }
+
+               
+                static void exibirDados(int[,] n)
+                {
+                    for (int i = 0; i < n.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < n.GetLength(1); j++)
+                        {
+                            Console.Write("{0:D2}\t", n[i, j]);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    
+
