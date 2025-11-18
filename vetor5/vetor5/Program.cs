375eca50d369d1b4513a676987492b47950d4a2a
Lavínia Ferreira <souzlaviniaferreira@gmail.com>
2025-11-18 14:53:27 -0300

    Add files via upload

---
+++ vetor5/vetor5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            const int tam = 10;
+            string[] times = new string[tam];
+
+            Console.WriteLine("*** Times de Futebol ***\n");
+
+            for (i = 0; i < tam; i++)
+            {
+                Console.Write("Nome do {0}º time: ", i + 1);
+                times[i] = Console.ReadLine();
+            }
+            Console.WriteLine("\n *** Times cadastrados no Vetor ***\n");
+            exibirDados(times);
+
+            Console.ReadKey();
+        }
+        static void exibirDados(string[] t)
+        {
+            for (int x = 0; x < t.Length; x++)
+                Console.WriteLine("{0}º time: {1}", x + 1, t[x]);
+        }
+    }
+     
+}
