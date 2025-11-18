89e1b0b342ffb888ca214a9cf5c99f690e833cd6
Lavínia Ferreira <souzlaviniaferreira@gmail.com>
2025-11-18 14:51:51 -0300

    Add files via upload

---
+++ vetor2/vetor2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("digite o {0}º nome:", i + 1);
+                nomes[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}º nome: {1}", i + 1, nomes[i]);
+            }
+            Console.ReadKey();
+            }
+        }
+}
