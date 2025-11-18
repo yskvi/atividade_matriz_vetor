4d9702dc2594a7664df47db2d69a6f4b66b0fca4
Lavínia Ferreira <souzlaviniaferreira@gmail.com>
2025-11-18 14:54:08 -0300

    Add files via upload

---
+++ xd 7/xd 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N_LIN = 3, N_COL = 3;
+            int[,] mat = new int[N_LIN, N_COL];
+            int l, c;
+            Console.WriteLine("Cadastrando dados na matriz\n");
+            Console.WriteLine("\nEntre com o valor em cada posição da matriz:\n");
+            for (l = 0; l < N_LIN; l++)
+            {
+                for (c = 0; c < N_COL; c++)
+                {
+                    Console.Write(" mat[{0}][{1}]=", l, c);
+                    mat[l, c] = int.Parse(Console.ReadLine());
+                } /*fim do for c*/
+            } /*fim do for l*/
+
+            Console.WriteLine("\n\nExibindo os dados da matriz:\n");
+            for (l = 0; l < N_LIN; l++)
+            {
+                for (c = 0; c < N_COL; c++)
+                {
+                    Console.Write(" {0} ", mat[l, c]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
