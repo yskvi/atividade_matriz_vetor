548b2526bbc30d001abfacea9ab820e67e004c53
Lavínia Ferreira <souzlaviniaferreira@gmail.com>
2025-11-18 14:52:23 -0300

    Add files via upload

---
+++ Vetor3/Vetor3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra;
+            int i;
+
+            Console.Write("Digite uma palavra: ");
+            palavra = Console.ReadLine();
+
+            Console.WriteLine("\nVogais da palavra: {0} \n", palavra);
+
+            char[] vetor = palavra.ToCharArray();
+
+            int tamanho = vetor.Length;
+
+            for (i = 0; i < tamanho; i++)
+            {
+                if (vetor[i] == 'a' || vetor[i] == 'e' || vetor[i] == 'i' || vetor[i] == 'o' || vetor[i] == 'u') 
+                    Console.Write(" {0}",vetor[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
