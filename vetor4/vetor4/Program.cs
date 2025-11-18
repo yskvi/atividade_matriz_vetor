b7fccf0494a98a30a0ac663a76ed0cd72ef84688
Lavínia Ferreira <souzlaviniaferreira@gmail.com>
2025-11-18 14:53:00 -0300

    Add files via upload

---
+++ vetor4/vetor4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double sal_minimo = 678.00;
+            double[] salarios = new double[10];
+            for (i = 0; i < salarios.Length; i++)
+                salarios[i] = (i + 1) * sal_minimo;
+            for (i = 0; i < salarios.Length; i++)
+                Console.WriteLine("\n {0} Salarios(s) Minimo(s) = {1}", (i + 1), salarios[i].ToString("#00.00"));
+            Console.ReadKey();
+        }
+    }
+}
