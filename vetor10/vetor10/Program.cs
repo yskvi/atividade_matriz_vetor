b79ec16b67c5a50ca101fe3a51cdcb61ad2a3a36
Lavínia Ferreira <souzlaviniaferreira@gmail.com>
2025-11-18 14:57:00 -0300

    Add files via upload

---
+++ vetor10/vetor10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                int l, c;
+                const int QTD_COL = 3;
+                const int QTD_LIN = 5;
+                string[,] nomes = new string[QTD_LIN, QTD_COL];
+
+                string n = "Fulano ";
+                int cont = 1;
+
+                
+                for (l = 0; l < QTD_LIN; l++)
+                {
+                    for (c = 0; c < QTD_COL; c++)
+                    {
+                        nomes[l, c] = n + cont++;
+                    }
+                }
+
+                Console.WriteLine("\n *** Nomes cadastrados na Matriz ***\n");
+                exibirDados(nomes);
+                Console.ReadKey();
+            }
+
+            static void exibirDados(string[,] m)
+            {
+                
+                for (int i = 0; i < m.GetLength(0); i++)
+                {
+                    for (int j = 0; j < m.GetLength(1); j++)
+                    {
+                        Console.Write("{0}\t", m[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+
